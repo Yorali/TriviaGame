47dc097543945c4df81d24b3c5212aa24d490c53
Yorali <hideayeni@gmail.com>
2017-06-10 15:27:22 -0500

    完了・Complete

--- javascript/app.js
+++ javascript/app.js
@@ -1,44 +1,4 @@
 
-var questions = [
-	{
-		question: "Question 1",
-		answers: ["A", "B", "Answer", "C"],
-		correctAnswer: "Answer"
-	},
-	{
-		question: "Question 2",
-		answers: ["Answer", "A", "B", "C"],
-		correctAnswer: "Answer"
-	},
-	{
-		question: "Question 3",
-		answers: ["A", "Answer", "B", "C"],
-		correctAnswer: "Answer"
-	},
-
-
-]
-
-que = [
-	"Question 1",
-	"Question 2",
-	"Question 3"
-
-];
-
-selection = [
-	["A", "B", "Answer", "C"],
-	["Answer", "A", "B", "C"],
-	["A", "Answer", "B", "C"]
-
-];
-
-correct = [
-	selection[0][2],
-	selection[1][0],
-	selection[2][1]
-];
-
 score = 0;
 right = 0;
 wrong = 0;
@@ -56,6 +16,10 @@ $(document).ready(function() {
 	$('#finishbutton').on('click', function() {
 		console.log('Number correct: ' + right);
 		console.log('Number incorrect: ' + wrong);
+		document.getElementById("visibi").style.visibility = 'hidden';
+		$('#numcorrect').html("Correct: " + right);
+		$('#numwrong').html("Incorrect: " + wrong)
+		document.getElementById("statspage").style.visibility = 'visible';
 	});
 
 	$('#startbutton').on('click',function() {
@@ -73,6 +37,7 @@ $(document).ready(function() {
 				clearInterval(countdown);
 				$('.timer').html('Game Over');
 				document.getElementById("visibi").style.visibility = 'hidden';
+				document.getElementById("statspage").style.visibility = 'visible';
 			};
 			$('.timer').html('Time left: ' + time + ' seconds');
 		};
